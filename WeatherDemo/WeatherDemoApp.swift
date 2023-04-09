09ba7ebd522416b9c3709e0896085e329848cd6c
akanksha <akanksha.shiraskar@edg.com.au>
2023-04-09 09:53:05 +0530

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Akanksha Shiraskar on 09/04/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
